4531084b7b4da2bcad5da34329a31de099a321de
globalpayredeban <dev@globalpay.com.co>
2021-03-16 13:48:31 -0600

    Update v2

--- gp-woocommerce-plugin.php
+++ gp-woocommerce-plugin.php
@@ -4,7 +4,7 @@
 Plugin Name: GlobalPay WooCommerce Plugin
 Plugin URI: https://developers.globalpay.com.co/docs/payments/
 Description: This module is a solution that allows WooCommerce users to easily process credit card payments.
-Version: 1.0
+Version: 2.0
 Author: GlobalPay
 Author URI: https://developers.globalpay.com.co/docs/payments/
 Text Domain: gp_woocommerce
@@ -13,54 +13,51 @@ License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
 
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	return;
+}
+
+define("GP_FLAVOR", "Globalpay");
+define("GP_DOMAIN", "globalpay.com.co");
+define("GP_REFUND", "/v2/transaction/refund/");
+define("GP_LTP", "/linktopay/init_order/");
+
 add_action( 'plugins_loaded', 'gp_woocommerce_plugin' );
 
-include( dirname( __FILE__ ) . '/includes/gp-woocommerce-helper.php' );
-register_activation_hook( __FILE__, array( 'WC_GlobalPay_Database_Helper', 'create_database' ) );
-register_deactivation_hook( __FILE__, array( 'WC_GlobalPay_Database_Helper', 'delete_database' ) );
+register_activation_hook( __FILE__, array( 'PG_WC_Helper', 'create_table' ) );
 
 require( dirname( __FILE__ ) . '/includes/gp-woocommerce-refund.php' );
 
 load_plugin_textdomain( 'gp_woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-define("GP_DOMAIN", "globalpay.com.co");
-define("GP_REFUND", "/v2/transaction/refund/");
-
-// TODO: Mover la function globalpay_woocommerce_order_refunded
-// define the woocommerce_order_refunded callback
-function globalpay_woocommerce_order_refunded($order_id, $refund_id) {
-  $refund = new WC_GlobalPay_Refund();
-  $refund->refund($order_id);
-}
-
-// add the action
-add_action( 'woocommerce_order_refunded', 'globalpay_woocommerce_order_refunded', 10, 2 );
-
 if (!function_exists('gp_woocommerce_plugin')) {
   function gp_woocommerce_plugin() {
-    class WC_Gateway_GlobalPay extends WC_Payment_Gateway {
+    class GP_WC_Plugin extends WC_Payment_Gateway {
       public function __construct() {
-        # $this->has_fields = true;
-        $this->id = 'gp_woocommerce';
-        $this->icon = apply_filters('woocomerce_globalpay_icon', plugins_url('/assets/imgs/favicon.ico', __FILE__));
-        $this->method_title = 'GlobalPay Plugin';
-        $this->method_description = __('This module is a solution that allows WooCommerce users to easily process credit card payments.', 'gp_woocommerce');
+        $this->id                 = 'gp_woocommerce';
+        $this->icon               = apply_filters('woocomerce_pg_icon', plugins_url('/assets/imgs/payment_checkout.ico', __FILE__));
+        $this->method_title       = GP_FLAVOR;
+        $this->method_description = __('This module is a solution that allows WooCommerce users to easily process credit card payments. Developed by: ', 'gp_woocommerce').GP_FLAVOR;
+				$this->supports           = array( 'products', 'refunds' );
 
         $this->init_settings();
         $this->init_form_fields();
 
-        $this->title = $this->get_option('title');
-        $this->description = $this->get_option('description');
+        $this->title              = $this->get_option('title');
+        $this->description        = $this->get_option('description');
 
-        $this->checkout_language = $this->get_option('checkout_language');
-        $this->enviroment = $this->get_option('staging');
+        $this->checkout_language  = $this->get_option('checkout_language');
+        $this->environment        = $this->get_option('staging');
+        $this->enable_ltp         = $this->get_option('enable_ltp');
+				$this->installments_type  = $this->get_option('installments_type');
 
-        $this->app_code_client = $this->get_option('app_code_client');
-        $this->app_key_client = $this->get_option('app_key_client');
-        $this->app_code_server = $this->get_option('app_code_server');
-        $this->app_key_server = $this->get_option('app_key_server');
+        $this->app_code_client    = $this->get_option('app_code_client');
+        $this->app_key_client     = $this->get_option('app_key_client');
+        $this->app_code_server    = $this->get_option('app_code_server');
+        $this->app_key_server     = $this->get_option('app_key_server');
 
-        // Para guardar sus opciones, simplemente tiene que conectar la función process_admin_options en su constructor.
+        $this->css                = plugins_url('/assets/css/styles.css', __FILE__);
+
         add_action('woocommerce_update_options_payment_gateways_' . $this->id, array(&$this, 'process_admin_options'));
 
         add_action('woocommerce_receipt_gp_woocommerce', array(&$this, 'receipt_page'));
@@ -71,89 +68,98 @@ if (!function_exists('gp_woocommerce_plugin')) {
       }
 
       function admin_options() {
-        $logo = plugins_url('/assets/imgs/logo.jpg', __FILE__);
+        $logo = plugins_url('/assets/imgs/payment.jpg', __FILE__);
         ?>
         <p>
           <img style='width: 30%;position: relative;display: inherit;'src='<?php echo $logo;?>'>
         </p>
-        <h2><?php _e('GlobalPay Gateway','gp_woocommerce'); ?></h2>
+        <h2><?php echo GP_FLAVOR.' Gateway'; ?></h2>
           <table class="form-table">
             <?php $this->generate_settings_html(); ?>
           </table>
         <?php
       }
 
-      function receipt_page($order) {
-        echo $this->generate_globalpay_form($order);
-      }
-
-      // TODO: Reposicionar la function get_params_post en otro archivo
-      public function get_params_post($orderId) {
+      function receipt_page($orderId) {
         $order = new WC_Order($orderId);
-        $order_data = $order->get_data();
-        $amount = $order_data['total'];
-        $products = $order->get_items();
-        $description = '';
-        foreach ($products as $product) {
-          $description .= $product['name'] . ',';
+        echo $this->generate_cc_form($order);
+        if ($this->enable_ltp == 'yes') {
+          echo $this->generate_ltp_form($order);
         }
-        $subtotal = number_format(($order->get_subtotal()), 2, '.', '');
-        $vat = number_format(($order->get_total_tax()), 2, '.', '');
-        if (is_null($order_data['customer_id']) or empty($order_data['customer_id'])) {
-            $uid = $orderId;
-        } else {
-            $uid = $order_data['customer_id'];
-        }
-        $parametersArgs = array(
-          'purchase_order_id'    => $orderId,
-          'purchase_amount'      => $amount,
-          'purchase_description' => $description,
-          'customer_phone'       => $order_data['billing']['phone'],
-          'customer_email'       => $order_data['billing']['email'],
-          'user_id'              => $uid,
-          'vat'                  => $vat
-        );
-
-        return $parametersArgs;
       }
 
-      public function generate_globalpay_form($orderId) {
-        $webhook_p = plugins_url('/includes/gp-woocommerce-webhook.php', __FILE__);
-        $css = plugins_url('/assets/css/styles.css', __FILE__);
-        $checkout = plugins_url('/assets/js/globalpay_checkout.js', __FILE__);
-        $orderData = $this->get_params_post($orderId);
-        $orderDataJSON = json_encode($orderData)
+			public function process_refund( $order_id, $amount = null,  $reason = '' ) {
+				$refund = new WC_Payment_Refund_GP();
+				$refund_data = $refund->refund($order_id, $amount);
+				if ($refund_data['success']) {
+					$order = new WC_Order($order_id);
+					$order->add_order_note( __('Transaction: ', 'gp_woocommerce') . $refund_data['transaction_id'] . __(' refund status: ', 'gp_woocommerce') . $refund_data['status'] . __(' reason: ', 'gp_woocommerce') . $reason);
+					return $refund_data['success'];
+				} else {
+					return $refund_data['success'];
+				}
+			}
+
+      public function generate_ltp_form($order) {
+        $url = PG_WC_Helper::generate_ltp($order, $this->environment);
+				$order->update_status( 'on-hold', __( 'Payment status will be updated via webhook.', 'gp_woocommerce' ) );
         ?>
-          <link rel="stylesheet" type="text/css" href="<?php echo $css; ?>">
+          <link rel="stylesheet" type="text/css" href="<?php echo $this->css; ?>">
+          <button id="ltp-button" class="<?php if($url == NULL){echo "hide";} else {echo "ltp-button";} ?>" onclick="ltpRedirect()">
+            <?php _e('Pay with Cash/Bank Transfer', 'gp_woocommerce'); ?>
+          </button>
+          <script>
+            function ltpRedirect() {
+              location.replace("<?php echo $url; ?>")
+            }
+          </script>
+        <?php
+      }
 
-          <div id="mensajeSucccess" class="hide"> <p class="alert alert-success" ><?php _e('Your payment has been made successfully. Thank you for your purchase.', 'gp_woocommerce'); ?></p> </div>
-          <div id="mensajeFailed" class="hide"> <p class="alert alert-warning"><?php _e('An error occurred while processing your payment and could not be made. Try another Credit Card.', 'gp_woocommerce'); ?></p> </div>
+      public function generate_cc_form($order) {
+        $webhook_p = plugins_url('/includes/gp-woocommerce-webhook.php', __FILE__);
+        $checkout = plugins_url('/assets/js/payment_checkout.js', __FILE__);
+        $order_data = PG_WC_Helper::get_checkout_params($order);
+        ?>
+          <link rel="stylesheet" type="text/css" href="<?php echo $this->css; ?>">
 
-          <div id="buttonreturn" class="hide">
+          <div id="msj-succcess" class="hide"> <p class="alert alert-success" ><?php _e('Your payment has been made successfully. Thank you for your purchase.', 'gp_woocommerce'); ?></p> </div>
+          <div id="msj-failed" class="hide"> <p class="alert alert-warning"><?php _e('An error occurred while processing your payment and could not be made. Try another Credit Card.', 'gp_woocommerce'); ?></p> </div>
+
+          <div id="button-return" class="hide">
             <p>
-              <a class="btn-tienda" href="<?php echo get_permalink( wc_get_page_id( 'shop' ) ); ?>"><?php _e( 'Return to Store', 'woocommerce' ) ?></a>
+              <a class="purchase-button" href="<?php echo get_permalink( wc_get_page_id( 'shop' ) ); ?>"><?php _e( 'Return to Store', 'gp_woocommerce' ) ?></a>
             </p>
           </div>
 
-          <script src="https://cdn.globalpay.com.co/ccapi/sdk/payment_checkout_2.0.0.min.js"></script>
+          <script src="https://cdn.globalpay.com.co/ccapi/sdk/payment_checkout_stable.min.js"></script>
 
-          <button class="js-payment-checkout"><?php _e('Purchase', 'gp_woocommerce'); ?></button>
+          <button id="checkout-button" class="js-payment-checkout"><?php _e('Pay With Card', 'gp_woocommerce'); ?></button>
 
-          <div id="orderDataJSON" class="hide">
-            <?php echo $orderDataJSON; ?>
+          <div id="order_data" class="hide">
+            <?php echo json_encode($order_data); ?>
           </div>
-          <script id="woocommerce_checkout_gp" webhook_p="<?php echo $webhook_p; ?>"
-            app-key="<?php echo $this->app_key_client; ?>"
-            app-code="<?php echo $this->app_code_client; ?>"
+
+          <script id="woocommerce_checkout_pg" webhook_p="<?php echo $webhook_p; ?>"
+            app_key="<?php echo $this->app_key_client; ?>"
+            app_code="<?php echo $this->app_code_client; ?>"
             checkout_language="<?php echo $this->checkout_language; ?>"
-            enviroment="<?php echo $this->enviroment; ?>"
+            environment="<?php echo $this->environment; ?>"
+						installments_type="<?php echo $this->installments_type; ?>"
             src="<?php echo $checkout; ?>">
           </script>
         <?php
       }
 
+      /**
+  		 * Process the payment and return the result
+  		 *
+  		 * @param int $orderId
+  		 * @return array
+  		 */
       public function process_payment($orderId) {
           $order = new WC_Order($orderId);
+					WC()->cart->empty_cart();
           return array(
               'result' => 'success',
               'redirect' => $order->get_checkout_payment_url(true)
@@ -164,7 +170,7 @@ if (!function_exists('gp_woocommerce_plugin')) {
 }
 
 function add_gp_woocommerce_plugin( $methods ) {
-    $methods[] = 'WC_Gateway_GlobalPay';
+    $methods[] = 'GP_WC_Plugin';
     return $methods;
 }
 
--- includes/admin/gp-woocommerce-settings.php
+++ includes/admin/gp-woocommerce-settings.php
@@ -2,52 +2,70 @@
 
 return array (
     'staging' => array(
-        'title' => __( 'Staging Enviroment', 'gp_woocommerce' ),
+        'title' => __( 'Staging Environment', 'gp_woocommerce' ),
         'type' => 'checkbox',
-        'label' => __( 'Use the GlobalPay Gateway staging environment.', 'gp_woocommerce' ),
+        'label' => __( 'Use staging environment in ', 'gp_woocommerce' ).GP_FLAVOR.'.',
         'default' => 'yes'
+    ),
+    'enable_ltp' => array(
+        'title' => __( 'Enable LinkToPay', 'gp_woocommerce' ),
+        'type' => 'checkbox',
+        'label' => __( 'If selected, LinkToPay(Bank transfer, cash) can be used to pay.', 'gp_woocommerce' ),
+        'default' => 'no'
     ),
     'title' => array(
         'title' => __( 'Title', 'gp_woocommerce' ),
         'type' => 'text',
-        'description' => __( 'This controls the title which the user sees during checkout.', 'gp_woocommerce' ),
-        'default' => __( 'GlobalPay Gateway', 'gp_woocommerce' ),
+        'description' => __( 'This controls the title which the user sees during checkout page.', 'gp_woocommerce' ),
+        'default' => GP_FLAVOR.' Gateway',
         'desc_tip' => true,
     ),
     'description' => array(
         'title' => __( 'Customer Message', 'gp_woocommerce' ),
         'type' => 'textarea',
-        'default' => __('GlobalPay is a complete solution for online payments. Safe, easy and fast.', 'gp_woocommerce')
+        'description' => __( 'This controls the message which the user sees during checkout page.', 'gp_woocommerce' ),
+        'default' => GP_FLAVOR.__(' is a complete solution for online payments. Safe, easy and fast.', 'gp_woocommerce
+        ')
     ),
     'checkout_language' => array(
-    'title' => __('Checkout Language', 'gp_woocommerce'),
-    'type' => 'select',
-    'default' => 'en',
-    'options' => array(
+      'title' => __('Checkout Language', 'gp_woocommerce'),
+      'type' => 'select',
+      'default' => 'en',
+      'options' => array(
         'en' => 'EN',
         'es' => 'ES',
         'pt' => 'PT',
+      ),
+      'description' => __('User\'s preferred language for checkout window. English will be used by default.', 'gp_woocommerce')
     ),
-    'description' => __('User\'s preferred language for checkout window. English will be used by default.', 'gp_woocommerce')
+    'installments_type' => array(
+      'title' => __('Installments Type', 'gp_woocommerce'),
+      'type' => 'select',
+      'default' => -1,
+      'options' => array(
+        -1 => __('Disabled', 'gp_woocommerce'),
+        0  => __('Enabled', 'gp_woocommerce'),
+      ),
+      'description' => __('Select the installments type that will be enabled on the payment screen (Only on card payment).', 'gp_woocommerce')
     ),
     'app_code_client' => array(
-    'title' => __('App Code Client', 'gp_woocommerce'),
-    'type' => 'text',
-    'description' => __('Unique identifier in GlobalPay.', 'gp_woocommerce')
+      'title' => __('App Code Client', 'gp_woocommerce'),
+      'type' => 'text',
+      'description' => __('Unique commerce identifier in ', 'gp_woocommerce').GP_FLAVOR.'.'
     ),
     'app_key_client' => array(
-        'title' => __('App Key Client', 'gp_woocommerce'),
-        'type' => 'text',
-        'description' => __('Key used to encrypt communication with GlobalPay.', 'gp_woocommerce')
+      'title' => __('App Key Client', 'gp_woocommerce'),
+      'type' => 'text',
+      'description' => __('Key used to encrypt communication with ', 'gp_woocommerce').GP_FLAVOR.'.'
     ),
     'app_code_server' => array(
-        'title' => __('App Code Server', 'gp_woocommerce'),
-        'type' => 'text',
-        'description' => __('Unique identifier in GlobalPay Server.', 'gp_woocommerce')
+      'title' => __('App Code Server', 'gp_woocommerce'),
+      'type' => 'text',
+      'description' => __('Unique commerce identifier to perform admin actions on ', 'gp_woocommerce').GP_FLAVOR.'.'
     ),
     'app_key_server' => array(
-        'title' => __('App Key Server', 'gp_woocommerce'),
-        'type' => 'text',
-        'description' => __('Key used for reverse communication with GlobalPay Server.', 'gp_woocommerce')
+      'title' => __('App Key Server', 'gp_woocommerce'),
+      'type' => 'text',
+      'description' => __('Key used to encrypt admin communication with ', 'gp_woocommerce').GP_FLAVOR.'.'
     )
   );
